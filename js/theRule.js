f83f26678545710357e2be705c3eed651a5d254c
Igor Correa <igorbuenocorrea@gmail.com>
2018-03-16 20:21:41 -0300

    Finishes basic functionality

--- js/theRule.js
+++ js/theRule.js
@@ -1,7 +1,42 @@
 function start() {
-
+  setupKeyResponse();
+  setAnswer();
 }
 
-function validate(n1, n2, n3) {
-  return (n1 < n2 && n2 < n3)
+function setAnswer() {
+  document.getElementById("answer-text").innerHTML =
+    getAnswerText(getSequence());
+}
+
+function getSequence() {
+  n = []
+  for(var i = 1; i <= 3; i++)
+    n.push(document.getElementById("n" + i).value);
+
+  return n;
+}
+
+function getAnswerText(n) {
+  for(var i = 0; i < n.length; i++) {
+    if(isNaN(Number(n[i])))
+      return n[i] + " is not really a number";
+    n[i] = Number(n[i]);
+  }
+
+  var valid = validate(n);
+
+  return (
+    n.toString() +
+    " is" +
+    (valid ? "" : " NOT") +
+    " a valid sequence, according to the rule"
+  );
+}
+
+function validate(n) {
+  for(var i = 0; i < n.length - 1; i++)
+    if(n[i] >= n[i + 1])
+      return false;
+
+  return true;
 }
